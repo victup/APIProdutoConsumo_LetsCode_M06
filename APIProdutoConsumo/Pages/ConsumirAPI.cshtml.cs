f1890a8c2eb1228f918d74b60873d5a06f54d556
Víctor Gonçalves <38474570+victup@users.noreply.github.com>
2022-09-09 22:02:43 -0300

    Add project files.

---
+++ APIProdutoConsumo/Pages/ConsumirAPI.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace APIProdutoConsumo.Pages
+{
+    public class ConsumirAPIModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ APIProdutoConsumo/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function MyTestCors(host, uri, methodName, includeHeaders = false) {
+    const resultSpan = document.getElementById('result');
+    const myHeaders = includeHeaders ? { 'x-victor': 'text/plain' } : {};
+
+    fetch(`${host}${uri}`,
+        {
+            method: methodName,
+            headers: myHeaders,
+        }).then(response => {
+            if (response.ok) {
+                response.text().then(text => {
+                    resultSpan.innerText = text;
+                });
+            }
+            else {
+                resultSpan.innerText = response.status;
+            }
+        })
+        .catch(() => resultSpan.innerText = 'See F12 Console for error');
+}
